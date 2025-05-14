5068e3f9792b4f5ffce2eb0857306986e0fc4b5d
unl1ke4 <w1tcher@ukr.net>
2025-05-14 21:42:30 +0300

    Вніс зміни для можливості перегляду сайту. Залишалась проблема з картинками на сайті.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Service
 
-# Create your views here.
+def home(request):
+    services = Service.objects.all()
+    return render(request, "index.html", {"services": services})
