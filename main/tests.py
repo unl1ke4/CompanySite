886e851b4154ab6d4c281a77ee418010a7f199a9
unl1ke4 <w1tcher@ukr.net>
2025-05-15 19:53:40 +0300

    Створив тест для перевірки роботи сторінки: чи видає помилки, чи вона взагалі існує.

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexPageTests(TestCase):
+    def test_index_page_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
