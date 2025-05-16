310d950e66fc7f0f4e1eb8a69b8eca5c4f74d0d1
unl1ke4 <w1tcher@ukr.net>
2025-05-16 06:37:04 +0300

    Додав localhost до ALLOWED_HOSTS.

--- company_site/settings.py
+++ company_site/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-ud1cka1fg)x-q60lo)&3j!4-u0+vd*jsbhu7hz&58et-9w2^^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["companysite-lylg.onrender.com",]
+ALLOWED_HOSTS = ["companysite-lylg.onrender.com",
+    "localhost",
+]
 
 
 # Application definition
